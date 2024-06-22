35d30aba68e4a86453babfa7846ebd4227f948c6
bordanDev <bordanproduction@gmail.com>
2024-06-22 22:56:26 +0300

    slider

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const previousSection = () => {
     const current = cards[currentCard];
     const previous = cards[currentCard - 1];
+
     validateButtons();
 
     if (!previous) {
@@ -11,6 +12,7 @@ const previousSection = () => {
     previous.classList.toggle('disabled', false);
 
     currentCard--;
+    updateScrollbar();
     validateButtons();
 }
 
@@ -28,23 +30,32 @@ const nextSection = () => {
     next.classList.toggle('disabled', false);
 
     currentCard++;
+    updateScrollbar();
     validateButtons();
 }
 
 const validateButtons = () => {
     if (cards[currentCard + 1]) {
         btnDown.style.display = 'unset';
+        btnUp.style.bottom = '161px';
     } else {
         btnDown.style.display = 'none';
+        btnUp.style.bottom = '72px';
     }
 
     if (cards[currentCard - 1]) {
         btnUp.style.display = 'unset';
     } else {
         btnUp.style.display = 'none';
+
     }
 }
 
+const updateScrollbar = () => {
+    scrollItem.forEach((item, index) => {
+        item.classList.toggle('active', index === currentCard);
+    });
+}
 
 const cards = Array.from(document.querySelectorAll('.header-section'))
 cards.slice(1).forEach(x => x.classList.add('disabled'))
@@ -53,7 +64,17 @@ let currentCard = 0;
 const btnUp = document.querySelector('#up');
 const btnDown = document.querySelector('#down');
 
+const scrollbar = document.querySelector('.scrollbar');
+
+
+const scrollItem = Array.from(document.querySelectorAll('.scroll'))
+// scrollItem.slice(1).forEach(x => x.classList.add(''))
+
+
+
 btnUp.addEventListener('click', () => previousSection());
 btnDown.addEventListener('click', () => nextSection());
 
 validateButtons();
+
+
