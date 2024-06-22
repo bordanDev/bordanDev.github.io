63e5e0b7786949d7dd9d6590b69804c05336674c
bordanDev <bordanproduction@gmail.com>
2024-06-22 23:31:28 +0300

    mouse scroll reaction

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/*
+
 const previousSection = () => {
     const current = cards[currentCard];
     const previous = cards[currentCard - 1];
@@ -78,3 +80,92 @@ btnDown.addEventListener('click', () => nextSection());
 validateButtons();
 
 
+*/
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const previousSection = () => {
+        const current = cards[currentCard];
+        const previous = cards[currentCard - 1];
+
+        validateButtons();
+
+        if (!previous) {
+            return;
+        }
+
+        current.classList.toggle('disabled', true);
+        previous.classList.toggle('disabled', false);
+
+        currentCard--;
+        updateScrollbar();
+        validateButtons();
+    }
+
+    const nextSection = () => {
+        const current = cards[currentCard];
+        const next = cards[currentCard + 1];
+        validateButtons();
+
+
+        if (!next) {
+            return;
+        }
+
+        current.classList.toggle('disabled', true);
+        next.classList.toggle('disabled', false);
+
+        currentCard++;
+        updateScrollbar();
+        validateButtons();
+    }
+
+    const validateButtons = () => {
+        if (cards[currentCard + 1]) {
+            btnDown.style.display = 'unset';
+            btnUp.style.bottom = '161px';
+        } else {
+            btnDown.style.display = 'none';
+            btnUp.style.bottom = '72px';
+        }
+
+        if (cards[currentCard - 1]) {
+            btnUp.style.display = 'unset';
+        } else {
+            btnUp.style.display = 'none';
+
+        }
+    }
+
+    const updateScrollbar = () => {
+        scrollItem.forEach((item, index) => {
+            item.classList.toggle('active', index === currentCard);
+        });
+    }
+
+    const cards = Array.from(document.querySelectorAll('.header-section'))
+    cards.slice(1).forEach(x => x.classList.add('disabled'))
+    let currentCard = 0;
+
+    const btnUp = document.querySelector('#up');
+    const btnDown = document.querySelector('#down');
+
+    const scrollbar = document.querySelector('.scrollbar');
+
+    const scrollItem = Array.from(document.querySelectorAll('.scroll'))
+
+    btnUp.addEventListener('click', () => previousSection());
+    btnDown.addEventListener('click', () => nextSection());
+
+    const handleScroll = (event) => {
+        if (event.deltaY < 0) {
+            previousSection();
+        } else if (event.deltaY > 0) {
+            nextSection();
+        }
+    };
+
+    document.addEventListener('wheel', handleScroll);
+
+    validateButtons();
+});
