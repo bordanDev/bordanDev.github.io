0b9426df2a3bf7bf6b7be8665ef71ee95a1933d6
bordanDev <bordanproduction@gmail.com>
2024-06-24 14:11:23 +0300

    3 & 4 section, fix updown btn

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-/*
-
 const previousSection = () => {
     const current = cards[currentCard];
     const previous = cards[currentCard - 1];
@@ -39,16 +37,18 @@ const nextSection = () => {
 const validateButtons = () => {
     if (cards[currentCard + 1]) {
         btnDown.style.display = 'unset';
-        btnUp.style.bottom = '161px';
+        btnUp.style.bottom = '140px';
     } else {
-        btnDown.style.display = 'none';
+        btnDown.style.display = 'unset';
         btnUp.style.bottom = '72px';
+        btnUp.classList.remove('up-downer');
     }
 
     if (cards[currentCard - 1]) {
         btnUp.style.display = 'unset';
     } else {
-        btnUp.style.display = 'none';
+        btnUp.classList.add('up-downer');
+        btnUp.style.bottom = '72px';
 
     }
 }
@@ -68,104 +68,19 @@ const btnDown = document.querySelector('#down');
 
 const scrollbar = document.querySelector('.scrollbar');
 
-
 const scrollItem = Array.from(document.querySelectorAll('.scroll'))
-// scrollItem.slice(1).forEach(x => x.classList.add(''))
-
-
 
 btnUp.addEventListener('click', () => previousSection());
 btnDown.addEventListener('click', () => nextSection());
 
+const handleScroll = (event) => {
+    if (event.deltaY < 0) {
+        previousSection();
+    } else if (event.deltaY > 0) {
+        nextSection();
+    }
+};
+
+document.addEventListener('wheel', handleScroll);
+
 validateButtons();
-
-
-*/
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const previousSection = () => {
-        const current = cards[currentCard];
-        const previous = cards[currentCard - 1];
-
-        validateButtons();
-
-        if (!previous) {
-            return;
-        }
-
-        current.classList.toggle('disabled', true);
-        previous.classList.toggle('disabled', false);
-
-        currentCard--;
-        updateScrollbar();
-        validateButtons();
-    }
-
-    const nextSection = () => {
-        const current = cards[currentCard];
-        const next = cards[currentCard + 1];
-        validateButtons();
-
-
-        if (!next) {
-            return;
-        }
-
-        current.classList.toggle('disabled', true);
-        next.classList.toggle('disabled', false);
-
-        currentCard++;
-        updateScrollbar();
-        validateButtons();
-    }
-
-    const validateButtons = () => {
-        if (cards[currentCard + 1]) {
-            btnDown.style.display = 'unset';
-            btnUp.style.bottom = '161px';
-        } else {
-            btnDown.style.display = 'none';
-            btnUp.style.bottom = '72px';
-        }
-
-        if (cards[currentCard - 1]) {
-            btnUp.style.display = 'unset';
-        } else {
-            btnUp.style.display = 'none';
-
-        }
-    }
-
-    const updateScrollbar = () => {
-        scrollItem.forEach((item, index) => {
-            item.classList.toggle('active', index === currentCard);
-        });
-    }
-
-    const cards = Array.from(document.querySelectorAll('.header-section'))
-    cards.slice(1).forEach(x => x.classList.add('disabled'))
-    let currentCard = 0;
-
-    const btnUp = document.querySelector('#up');
-    const btnDown = document.querySelector('#down');
-
-    const scrollbar = document.querySelector('.scrollbar');
-
-    const scrollItem = Array.from(document.querySelectorAll('.scroll'))
-
-    btnUp.addEventListener('click', () => previousSection());
-    btnDown.addEventListener('click', () => nextSection());
-
-    const handleScroll = (event) => {
-        if (event.deltaY < 0) {
-            previousSection();
-        } else if (event.deltaY > 0) {
-            nextSection();
-        }
-    };
-
-    document.addEventListener('wheel', handleScroll);
-
-    validateButtons();
-});
