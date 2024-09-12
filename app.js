b589e09ebee33759efd6501ddd66392cf038c4ab
bordanDev <bordanproduction@gmail.com>
2024-09-12 20:56:52 +0300

    upgrade adaptive design

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const cards = Array.from(document.querySelectorAll('.header-section'))
+cards.slice(1).forEach(x => x.classList.add('disabled'))
+let currentCard = 0;
+
+const btnUp = document.querySelector('#up');
+const btnDown = document.querySelector('#down');
+
+
 const previousSection = () => {
     const current = cards[currentCard];
     const previous = cards[currentCard - 1];
@@ -57,7 +65,33 @@ const updateScrollbar = () => {
     scrollItem.forEach((item, index) => {
         item.classList.toggle('active', index === currentCard);
     });
+
+    // if (window.innerWidth <= 479) {
+    //     if (cards[2].classList[2] !== 'disabled') {
+    //         scrollItem[2].innerHTML = '<div class="sub-active"></div>'
+    //         if (scrollItem[2].innerHTML === '<div class="scroll sub-active"></div>') {
+    //             const innerSubScroll = scrollItem[2].innerHTML
+    //             innerSubScroll.style.height = '50%'
+    //             innerSubScroll.style.backgroundcolor = '$gray-900 !important'
+    //             innerSubScroll.style.borderRadius = '20px'
+    //         }
+    //     }
+    // }
 }
+
+
+const listOfWorkpattern = Array.from(document.querySelectorAll('.workpattern-text'))
+
+
+const firstWorkpattern = listOfWorkpattern.slice(0, 4)
+
+const secondWorkpattern = listOfWorkpattern.slice(4, 8)
+
+// secondWorkpattern.forEach((element) => element.style.display = 'none')
+
+secondWorkpattern.forEach((element) => element)
+
+listOfWorkpattern.forEach(x => x.innerHTML)
 
 function menuOpener() {
     if (mobileMenu.style.left == '-100vw') {
@@ -67,13 +101,6 @@ function menuOpener() {
 
     mobileMenu.style.left = '-100vw'
 }
-
-const cards = Array.from(document.querySelectorAll('.header-section'))
-cards.slice(1).forEach(x => x.classList.add('disabled'))
-let currentCard = 0;
-
-const btnUp = document.querySelector('#up');
-const btnDown = document.querySelector('#down');
 
 const scrollbar = document.querySelector('.scrollbar');
 
@@ -100,3 +127,19 @@ const btnMenu = document.querySelector('.header-btn-mobile-menu')
 const mobileMenu = document.querySelector('.header-mobile-menu')
 
 btnMenu.addEventListener('click', () => menuOpener())
+
+
+// const listWorkpattern = document.querySelector('workpattern-list')
+// const itemWorkpattern = document.querySelectorAll('workpattern-text')
+
+// const points = Array.from(itemWorkpattern)
+
+// points.forEach(x => x.classList.add('disabled'))
+
+// itemWorkpattern.forEach(x => )
+
+
+// scrollItem[2].innerHTML = '<div class="sub-active"></div>'
+// const innerBlock = scrollItem[2].parentElement
+// scrollItem[2].style.backgroundColor = '#EBEBEB !important'
+// document.querySelector('sub-active').
